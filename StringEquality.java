1577831a29e634a8b125f5558bb5f65fb578c696
MaheshSade <85116033+MaheshSade@users.noreply.github.com>
2023-03-09 10:44:39 +0530

    Add files via upload

---
+++ StringEquality.java
@@ -0,0 +1,16 @@
+package org.leetcode;
+
+public class StringEquality {
+    public static void main(String[] args) {
+        String s1 = "s";
+        String s2 = "s";
+        String s3 = new String("s");
+        String s4 = new String("s");
+        System.out.println(s1.equals(s2));        //true
+        System.out.println(s1.equals(s3));        //true
+        System.out.println(s3.equals(s4));        //true
+        System.out.println(s1==s2);               //true
+        System.out.println(s3==s4);               //false
+        System.out.println(s1==s3);               //false
+    }
+}
