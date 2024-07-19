1a3ac930fad08b1dfea047c45d527694a41caf27
MaheshSade <85116033+MaheshSade@users.noreply.github.com>
2024-07-19 18:22:11 +0530

    Add files via upload

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+public class SelectionSort {
+
+    public void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length-2; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]){
+                    swap(arr,i,j);
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+
+    public void swap(int[] arr,int a, int b) {
+        arr[a] = arr[a] + arr[b];
+        arr[b] = arr[a] - arr[b];
+        arr[a] = arr[a] - arr[b];
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 95, 5, 95, 8, 6, 52};
+
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.selectionSort(arr);
+    }
+}
