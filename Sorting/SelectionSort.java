96fdaf1594414891debe4570b974b9d514f28731
MaheshSade <85116033+MaheshSade@users.noreply.github.com>
2024-09-13 09:51:59 +0530

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,7 +1,7 @@
 public class SelectionSort {
 
     public void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length-2; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[i] > arr[j]){
                     swap(arr,i,j);
